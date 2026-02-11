0c6bea2f77b6c398d6bc0435af8aa4443af1adff
Rohitshah507 <shahaaditya1111@gmail.com>
2026-02-11 21:04:13 +0545

    Updating the code

--- src/Routes/packageRoute.js
+++ src/Routes/packageRoute.js
@@ -1,23 +1,48 @@
 import express from "express";
+import multer from "multer";
 import auth from "../Middleware/auth.js";
 import packageController from "../Controller/packageController.js";
 import roleBasedAuth from "../Middleware/roleBasedAuth.js";
 
 const router = express.Router();
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 router.post(
   "/",
   auth,
   roleBasedAuth("GUIDE", "ADMIN"),
+  upload.array("images", 5),
   packageController.createPackage,
 );
 
-router.get("/package", auth, roleBasedAuth("TOURIST"), packageController.getAllPackages);
+router.get(
+  "/package",
+  auth,
+  roleBasedAuth("TOURIST", "GUIDE"),
+  packageController.getAllPackages,
+);
 
-router.get("/:id", auth, roleBasedAuth("TOURIST"),packageController.getPackageById);
+router.get(
+  "/:id",
+  auth,
+  roleBasedAuth("TOURIST", "GUIDE"),
+  packageController.getPackageById,
+);
 
-router.put("/:id", auth, roleBasedAuth("GUIDE"), packageController.updatePackage);
+router.put(
+  "/:id",
+  auth,
+  roleBasedAuth("GUIDE"),
+  upload.array("images", 5),
+  packageController.updatePackage,
+);
 
-router.delete("/:id", auth, roleBasedAuth("GUIDE"), packageController.deletePackage);
+router.delete(
+  "/:id",
+  auth,
+  roleBasedAuth("GUIDE"),
+  packageController.deletePackage,
+);
 
 export default router;
